055349977e7f8521855097196ce6a0bc53bf2d1c
Rachel Schieferstein <violenza@gmail.com>
2020-03-18 15:43:45 -0700

    adding delete route to restaurants controller
    Co-authored-by: Alyssa Colistro <arcolistro@gmail.com>
    Co-authored-by: K Wicz <katy@solovewicz.com>

--- BestRestaurants/Controllers/RestaurantsController.cs
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -57,5 +57,22 @@ namespace BestRestaurants.Controllers
       return RedirectToAction("Index");
     }
 
+  public ActionResult Delete(int id)
+  {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaraunts => restaraunts.RestaurantId == id);
+      return View(thisRestaurant);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+  }
+
+
+
   }
 }
--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -11,7 +11,7 @@ namespace BestRestaurants.Models
 
     public int Rating {get ; set; }
     public string Description { get; set; }
-    // public int CuisineId { get; set; }
-    // public virtual Cuisine Cuisine { get; set; }
+    public int CuisineId { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
   }
 }
